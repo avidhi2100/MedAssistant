ef3ae5de0409d7846051b8b7f8eb69e57ba49331
Harshil Vyas <54691846+Harshil-V@users.noreply.github.com>
2024-04-26 01:15:35 -0700

    Setup the UI for OCR

--- app/src/main/java/com/example/medassistant/OCR.java
+++ app/src/main/java/com/example/medassistant/OCR.java
@@ -9,14 +9,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class OCR extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(String.valueOf(this.getBaseContext()), "onCreate: Main");
+        Log.d(String.valueOf(this.getBaseContext()), "onCreate: OCR");
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_ocr);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
