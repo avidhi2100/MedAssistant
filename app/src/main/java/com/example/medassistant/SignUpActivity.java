75f2f31661eee1d17ecd14db347324ec7b50f789
aishwarya <aishwarya.manoharan@sjsu.edu>
2024-04-28 14:05:02 -0700

    Adding the sign up and login page along with the required libraries

--- app/src/main/java/com/example/medassistant/SignUpActivity.java
+++ app/src/main/java/com/example/medassistant/SignUpActivity.java
@@ -4,15 +4,10 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,11 +19,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class SignUp extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
 
     private FirebaseAuth auth;
-    private EditText signupEmail, signupPassword;
+    private EditText signupEmail, signupPassword,confirmPassword;
     private Button signupButton;
     private TextView loginRedirectText;
     @Override
@@ -39,6 +34,7 @@ public class SignUp extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         signupEmail = findViewById(R.id.signup_email);
         signupPassword = findViewById(R.id.signup_password);
+        confirmPassword=findViewById(R.id.signup_confirm_password);
         signupButton = findViewById(R.id.signup_button);
         loginRedirectText = findViewById(R.id.loginRedirectText);
 
@@ -47,13 +43,21 @@ public class SignUp extends AppCompatActivity {
             public void onClick(View view) {
                 String user = signupEmail.getText().toString().trim();
                 String pass = signupPassword.getText().toString().trim();
+                String confPass=confirmPassword.getText().toString().trim();
 
                 if (user.isEmpty()){
                     signupEmail.setError("Email cannot be empty");
                 }
                 if(pass.isEmpty()){
                     signupPassword.setError("Password cannot be empty");
-                } else{
+                }
+                if (confPass.isEmpty()){
+                    confirmPassword.setError("Confirm Password cannot be empty");
+                }
+                if (!(pass.equals(confPass))){
+                    confirmPassword.setError("Passwords does not match");
+                }
+            else{
                     auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
